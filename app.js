088c07586118118b5cff16615f0e7707f388100c
ElvisGT <elvisgiltorres@gmail.com>
2022-12-18 21:41:28 -0500

    Configurando los endpoints bases

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const express = require("express");
+import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 8081;
+
+app.get('/',(req,res) => {
+    res.json({
+        msg:"obteniendo empleados"
+    })
+})
+
+app.put('/',(req,res) => {
+    res.json({
+        msg:"actualizando empleados"
+    })
+})
+
+app.delete('/',(req,res) => {
+    res.json({
+        msg:"eliminando empleados"
+    })
+})
+
+app.post('/',(req,res) => {
+    res.json({
+        msg:"creando empleados"
+    })
+})
 
 app.listen(port,() => {
 
-    console.log("Server corriendo en el puerto: ",port);
+    console.log("Server corriendo en el puerto:",port);
 
 })
