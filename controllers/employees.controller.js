4533ade0900d81d5a29b4a1ec1bc4f1b7cb3f459
ElvisGT <elvisgiltorres@gmail.com>
2022-12-19 09:45:46 -0500

    Realizando validaciones en el controller de employee, crud finalizado

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -3,14 +3,25 @@ import {pool} from '../db.js';
 
 const getEmployees = async(req,res) => {
     const [results] = await pool.query('SELECT * FROM employee');
+    const total = results.length;
 
     res.json({
+        total,
         results
     })
 }
 
 const createEmployee = async(req,res) => {
     const {name,salary} = req.body;
+    const lowerName = name.toLowerCase();
+    const isEmployee = await pool.query("SELECT * FROM employee where name = ?",[lowerName]);
+    
+    if(isEmployee[0].length > 0){
+        return res.status(400).json({
+            msg:"El nombre del empleado ya existe"
+        })
+    }
+    
     await pool.query("INSERT INTO employee(name,salary) VALUES (?,?)",[name,salary]) 
     res.json({
         ok:true,
@@ -18,18 +29,31 @@ const createEmployee = async(req,res) => {
     })
 }
 
-const updateEmployee = (req,res) => {
+const updateEmployee = async (req,res) => {
+    const id = req.params.id;
+    const {name,salary} = req.body;
+    const lowerName = name.toLowerCase();
+    const isEmployee = await pool.query("SELECT * FROM employee where name = ?",[lowerName]);
+    
+    if(isEmployee[0].length > 0){
+        return res.status(400).json({
+            msg:"El nombre del empleado ya existe"
+        })
+    }
+    await pool.query('UPDATE employee set name = ?,salary = ? where id = ?;',[name,salary,id])
+    
     res.json({
-        msg:"Actualizando empleados"
+        ok:true,
+        msg:"Actualizado correctamente"
     })
 }
 
 const deleteEmployee = async (req,res) => {
     const id = req.params.id;
-    const employee = await pool.query('DELETE FROM employee where id = ?',[id]);
+    await pool.query('DELETE FROM employee where id = ?',[id]);
 
     res.json({
-        employee
+        msg:"Eliminado exitosamente"
     })
 }
 
