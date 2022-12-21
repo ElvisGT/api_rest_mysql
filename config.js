156b1671b33b31dd27f73ec32f6be7ea7412a55e
ElvisGT <elvisgiltorres@gmail.com>
2022-12-21 14:49:55 -0500

    Haciendo modificaciones en la configuracion de la base de datos

--- config.js
+++ config.js
@@ -5,8 +5,12 @@ const PORT = process.env.PORT || 8081;
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'elvisgt99';
 const DB_DATABASE = process.env.DB_DATABASE || 'companydb';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 3306;
 
 export {PORT,
         DB_USER,
         DB_DATABASE,
-        DB_PASSWORD}
+        DB_PASSWORD,
+        DB_HOST,
+        DB_PORT}
--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 import mysql2 from 'mysql2/promise';
 import {DB_DATABASE,
+        DB_HOST,
         DB_PASSWORD,
+        DB_PORT,
         DB_USER} from './config.js';
 
 
 export const pool = mysql2.createPool({
+    host:DB_HOST,
     database:DB_DATABASE,
     user:DB_USER,
-    password:DB_PASSWORD
+    password:DB_PASSWORD,
+    port:DB_PORT
 })
